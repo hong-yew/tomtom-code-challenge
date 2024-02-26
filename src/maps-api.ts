e65387940b965b7f560567ebec20718a48be3f8e
Hong Yew <>
2024-02-26 16:57:41 +1100

    remove unnecessary single quote

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -8,7 +8,7 @@ export type PlaceAddress = {
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
 export async function getPlaceAutocomplete(key: string, address: string): Promise<PlaceAddress[]> {
-    const autocomplete: AxiosResponse<TomTomSearchResult> = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
+    const autocomplete: AxiosResponse<TomTomSearchResult> = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json`, {
         params: {
             key,
             limit: 100,
